2c7ef8fb512b0609aafbd4ae3d962068b88fd1e9
root <root@ip-172-31-45-244.eu-west-1.compute.internal>
2016-12-12 21:02:37 +0000

    updatged gen

--- gen_mask.py
+++ gen_mask.py
@@ -1,21 +1,18 @@
 import scipy.misc
 import numpy as np
+import pickle
 
 def get_sat_mask(image):
-    return np.where( image > 255 , 1, 0)
+    return np.where( image > 0.99 , 1, 0)
 
 data = pickle.load( open('hl.p', 'rb') )
 
 x_source, y_source = data['train'];
 x_test, y_test = data['test'];
 
+mask = []
 #Loop over all images
-for i in xrange(0, x_test.shape[0]):
+for i in range(0, x_test.shape[0]):
     mask = get_sat_mask(x_test[i,:,:])
-    print(mask.shape)
-
-data = np.zeros((256,256), np.int8)
-for i in range(128):
-  for j in range(128):
-    data[i, j] = np.random.randint(2, size=1)
-scipy.misc.imsave('mask.png', data)
+    scipy.misc.imsave('itm_images/img{:4d}.png'.format(i) , x_test[i, :, :])
+    scipy.misc.imsave('itm_images/mask{:4d}.png'.format(i) , mask)
